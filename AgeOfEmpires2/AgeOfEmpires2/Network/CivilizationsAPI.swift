2500b004d27f3179847809a25d1e595acfe939ff
Jordi Milla Catalán <jb.90@hotmail.es>
2020-11-13 19:01:14 +0100

    Update Network.

--- AgeOfEmpires2/AgeOfEmpires2/Network/CivilizationsAPI.swift
+++ AgeOfEmpires2/AgeOfEmpires2/Network/CivilizationsAPI.swift
@@ -7,3 +7,54 @@
 //
 
 import Foundation
+import Moya
+
+enum CivilizationsAPI {
+    case getCivilization
+}
+
+extension CivilizationsAPI:TargetType,AccessTokenAuthorizable {
+    public var baseURL: URL {  return URL(string: "https://age-of-empires-2-api.herokuapp.com/api/v1/")!}
+    
+    public var path: String {
+        switch self {
+        case .getCivilization:
+            return "civilizations"
+        }
+        
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getCivilization:
+            return .get
+            
+        }
+    }
+    
+    var authorizationType: AuthorizationType {
+        switch self {
+        case .getCivilization:
+            return .bearer
+        }
+    }
+    
+    public var task: Task {
+        switch self {
+        case .getCivilization:
+            return .requestPlain
+        }
+    }
+    
+    public var validationType: ValidationType {
+        return .none
+    }
+    
+    public var sampleData: Data {
+        return "".data(using: String.Encoding.utf8)!
+    }
+    
+    public var headers: [String: String]? {
+        return nil
+    }
+}
