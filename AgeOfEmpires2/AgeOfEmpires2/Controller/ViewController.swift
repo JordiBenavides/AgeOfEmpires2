53075b50c8df7ba187f79f46891c3297cc0be67c
Jordi Milla Catalán <jb.90@hotmail.es>
2020-11-13 19:18:38 +0100

    Add the requestCivilization function and check that the data arrives.

--- AgeOfEmpires2/AgeOfEmpires2/Controller/ViewController.swift
+++ AgeOfEmpires2/AgeOfEmpires2/Controller/ViewController.swift
@@ -6,15 +6,33 @@
 //  Copyright © 2020 Jordi Milla Catalan. All rights reserved.
 //
 
+import Foundation
 import UIKit
+import Moya
 
 class ViewController: UIViewController {
+    
+    let provider = MoyaProvider<CivilizationsAPI>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        requestCivilization()
     }
 
+    func requestCivilization() {
+        
+        provider.request(.getCivilization) { result in
+            switch result {
+            case .success(let response):
+                print("success")
+//                let array = try! response.map([Movie].self)
+//                self.data = array
+//                self.tableView.reloadData()
+            case .failure:
+                print("Error")
+            }
+        }
+    }
 
 }
 
